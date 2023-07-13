0133f868e9b62cc5fa8ac57292d3d643ba8641d9
zen-manderson <michael.anderson@zenbusiness.com>
2023-07-12 21:18:29 -0500

    first commit

---
+++ src/main/kotlin/com/zenbusiness/kotlinordersapi/KotlinOrdersApiApplication.kt
@@ -0,0 +1,11 @@
+package com.zenbusiness.kotlinordersapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinOrdersApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinOrdersApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/zenbusiness/kotlinordersapi/KotlinOrdersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zenbusiness.kotlinordersapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinOrdersApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
